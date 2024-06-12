0c78b3d0eef7ae6f738039d384e6a6553d6e6c31
basrioglumehmet <basrioglumehmet@gmail.com>
2024-06-12 09:18:49 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ref_type_method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fullName = "mehmet basrioglu";
+            ProgramUtil.ReverseString(ref fullName);
+            Console.WriteLine(fullName); // Tersine çevrilmiş string yazdırılır
+        }
+    }
+
+    class ProgramUtil
+    {
+        // Stringler birer char dizisidir.
+        public static void ReverseString(ref string text) //Referans Tipler
+        {
+            if (text == null) return;
+            char[] array = text.ToCharArray();
+            Array.Reverse(array);
+            text = new string(array); // Orijinal string ref ile değiştirilir
+        }
+    }
+}
